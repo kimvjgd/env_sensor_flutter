1fd3ad944be667cf98295cd57127ca99160867d0
MinyeongHong <agka00161@gmail.com>
2023-05-05 23:44:23 +0900

    ui base

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,11 +1,51 @@
 import 'package:flutter/material.dart';
 
+import '../widget/main_tile.dart';
+
 class MainPage extends StatelessWidget {
   const MainPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("미녕쿤의 메인화면"),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.contrast),
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: const Icon(Icons.settings),
+              onPressed: () {},
+            ),
+          ],
+        ),
+        body: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Container(
+                  child: Text('32.4도'),
+                ),
+                Container(
+                  child: Text('46.8%'),
+                ),
+              ],
+            ),
+            Expanded(
+              child: GridView.count(
+                crossAxisCount: 7, // 가로 칸 수
+                childAspectRatio: 0.9, // 가로/세로 비율
+                children: List.generate(
+                  14, // 총 그리드뷰 아이템 수 (7 x 2)
+                      (index) => MainTile()
+                ),
+              ),
+            )
+          ],
+        ));
   }
 }
 
