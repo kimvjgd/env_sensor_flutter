1262273bf57afb9227fca1df751c96eeec38ff9c
kimvjgd <kimvjgd@naver.com>
2023-05-06 22:50:26 +0900

    google services file added

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,13 @@ import 'package:env_sensor/pages/splash_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+// import 'firebase_options.dart';
 import 'app_theme.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
+    // options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(const MyApp());
 }
